796e2301c109a8216d0a558cd83386f845a353cd
Rashminayakranjan99 <rashminayakranjan99@gmail.com>
2025-08-21 22:51:51 +0530

    Initial commit

--- script.js
+++ script.js
@@ -101,3 +101,16 @@ function createParticles() {
 
 // Initialize particles
 createParticles();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    target.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+                }
+            });
+        });
+        </script>
